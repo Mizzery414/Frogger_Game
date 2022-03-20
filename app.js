9415a9ea93243a3a328a967af7efbc5faa999c9f
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2022-03-19 17:25:59 -0700

    move frog element with key stroke

--- app.js
+++ app.js
@@ -4,21 +4,26 @@ const startPauseButton = document.querySelector("#start-pause-button");
 const squares = document.querySelectorAll(".grid div");
 
 let currentIndex = 76; //starting block index is 76
-console.log("squares");
+let width = 9; // nine squares from css setting
 
 function moveFrog(e) {
   switch (e.key) {
     case "ArrowLeft":
       console.log("move left");
+      currentIndex -= 1;
       break;
     case "ArrowRight":
       console.log("move right");
+      currentIndex += 1;
       break;
     case "ArrowUp":
       console.log("move up");
+      currentIndex -= width;
       break;
     case "ArrowDown":
       console.log("move down");
+      currentIndex += width;
+      break;
   }
 
   squares[currentIndex].classList.add("frog");
