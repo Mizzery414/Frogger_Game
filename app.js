fb33d7718058a6465a3d5654d9a8123e8526c1b9
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2022-03-19 17:21:24 -0700

    move key switch case add

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ const resultDisplay = document.querySelector("#result");
 const startPauseButton = document.querySelector("#start-pause-button");
 const squares = document.querySelectorAll(".grid div");
 
-let currentIndex = 0;
+let currentIndex = 76; //starting block index is 76
+console.log("squares");
 
-function moveFrog() {
-  console.log("moved");
+function moveFrog(e) {
+  switch (e.key) {
+    case "ArrowLeft":
+      console.log("move left");
+      break;
+    case "ArrowRight":
+      console.log("move right");
+      break;
+    case "ArrowUp":
+      console.log("move up");
+      break;
+    case "ArrowDown":
+      console.log("move down");
+  }
+
   squares[currentIndex].classList.add("frog");
 }
 
