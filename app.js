0251a5663c5186b6ce41f9819a600663170f43d4
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2022-03-21 17:35:48 -0700

    update styling

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
 const startPauseButton = document.querySelector("#start-pause-button");
+const replayBtn = document.querySelector("#replay-button");
 const squares = document.querySelectorAll(".grid div");
 const logsLeft = document.querySelectorAll(".log-left");
 const logsRight = document.querySelectorAll(".log-right");
 const carsLeft = document.querySelectorAll(".car-left");
 const carsRight = document.querySelectorAll(".car-right");
-const replayBtn = document.querySelector("#replay-button");
 
 let currentIndex = 76; //starting block index is 76
 let width = 9; // nine squares from css setting
